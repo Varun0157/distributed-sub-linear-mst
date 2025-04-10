e27297240e7d28acefd3b0a6b59a4e79c4d94d84
Varun0157 <varun.edachali@gmail.com>
2025-04-10 15:14:16 +0530

    feat: moe calculation

--- src/utils/graph.go
+++ src/utils/graph.go
@@ -14,6 +14,41 @@ func CreateAdjacencyList(edges []*Edge) map[int][]EdgeTarget {
 	return adjacencyList
 }
 
+// getMoE returns the minimum outgoing edge for each fragment given the current
+// graph and fragment ids
+func getMoE(adjacencyList map[int][]EdgeTarget, fragmentIds map[int]int) map[int]*Edge {
+	getMinOutgoingTargetEdge := func(src int, targets []EdgeTarget) *Edge {
+		var minEdge *Edge = nil
+
+		for _, target := range targets {
+			if fragmentIds[src] == fragmentIds[target.Dest] {
+				continue
+			}
+			if minEdge != nil && minEdge.Weight <= target.Weight {
+				continue
+			}
+			minEdge = NewEdge(src, target.Dest, target.Weight)
+		}
+
+		return minEdge
+	}
+
+	moes := make(map[int]*Edge)
+	for src, targets := range adjacencyList {
+		minEdge := getMinOutgoingTargetEdge(src, targets)
+		if minEdge == nil {
+			continue
+		}
+
+		fragment := fragmentIds[minEdge.Src]
+		if currMin, ok := moes[fragment]; !ok || currMin.Weight > minEdge.Weight {
+			moes[fragment] = minEdge
+		}
+	}
+
+	return moes
+}
+
 func GetEdgeList(adjacencyList map[int][]EdgeTarget) []*Edge {
 	edges := make([]*Edge, 0)
 	for src, targets := range adjacencyList {
